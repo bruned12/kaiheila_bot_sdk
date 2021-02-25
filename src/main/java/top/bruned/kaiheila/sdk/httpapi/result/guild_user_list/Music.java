7f73bbd5558905009d7c127f4b68113fb01912bf
bruned <2136407907@qq.com>
2021-02-26 01:16:13 +0800

    Update README.md

---
+++ src/main/java/top/bruned/kaiheila/sdk/httpapi/result/guild_user_list/Music.java
@@ -0,0 +1,46 @@
+/**
+  * Copyright 2021 bejson.com 
+  */
+package top.bruned.kaiheila.sdk.httpapi.result.guild_user_list;
+
+/**
+ * Auto-generated: 2021-02-21 17:15:48
+ *
+ * @author bejson.com (i@bejson.com)
+ * @website http://www.bejson.com/java2pojo/
+ */
+public class Music {
+
+    private String software;
+    private String music_name;
+    private String singer;
+    private String start_time;
+    public void setSoftware(String software) {
+         this.software = software;
+     }
+     public String getSoftware() {
+         return software;
+     }
+
+    public void setMusic_name(String music_name) {
+         this.music_name = music_name;
+     }
+     public String getMusic_name() {
+         return music_name;
+     }
+
+    public void setSinger(String singer) {
+         this.singer = singer;
+     }
+     public String getSinger() {
+         return singer;
+     }
+
+    public void setStart_time(String start_time) {
+         this.start_time = start_time;
+     }
+     public String getStart_time() {
+         return start_time;
+     }
+
+}
